4ddc9c871f90003c1ca930a0a72bda07986a7db0
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2024-09-25 16:25:12 +0530

    Committed the theme app windows

---
+++ ThemeAppWindows/ThemeAppWindows/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ThemeAppWindows
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ThemeAppWindows/ThemeAppWindows/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace ThemeAppWindows
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if(Application.Current is not null)
+            {
+                if (Application.Current.RequestedTheme == AppTheme.Dark)
+                {
+                    Application.Current.UserAppTheme = AppTheme.Light;
+                }
+                else
+                {
+                    Application.Current.UserAppTheme = AppTheme.Dark;
+                }
+            }
+           
+        }
+
+
+    }
+
+}
